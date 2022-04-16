28c2cc5db6c9d075e85335071e1df279b36501c5
Jessica Rozendaal <remyenjessica@gmail.com>
2022-04-16 18:11:51 +0200

    navigatie-klaar

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import './Home.css';
+import Navbar from "../../components/navigation/Navbar";
 
 const Home = () => {
     return (
         <div>
+            <Navbar />
         </div>
     );
 };
