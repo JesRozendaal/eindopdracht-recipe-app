27486d5149b6a9fa5a71b66236aa0d1176f7a38f
Jessica Rozendaal <remyenjessica@gmail.com>
2022-04-16 17:43:03 +0200

    routing-klaar

--- src/pages/login-register/LoginRegister.js
+++ src/pages/login-register/LoginRegister.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Fridge = () => {
+const LoginRegister = () => {
     return (
         <div>
 
@@ -8,4 +8,4 @@ const Fridge = () => {
     );
 };
 
-export default Fridge;
+export default LoginRegister;
